a0971ba3b763d4b6e3a71c415b8d8fb86ae034bd
황원태 <amlk31255@gmail.com>
2022-07-27 10:46:56 +0900

    set: Web3OJ

--- hardhat.config.js
+++ hardhat.config.js
@@ -24,11 +24,6 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
     // rinkeby
     rinkeby: {
       url: process.env.RINKEBY_URL || "https://rinkeby.infura.io/v3/",
--- scripts/plusCalculatorSol.js
+++ scripts/plusCalculatorSol.js
@@ -2,23 +2,27 @@ const { ethers } = require("hardhat");
 const hre = require("hardhat");
 
 async function calculatorSol() {
-    const [myAccount] = await ethers.getSigners();
-    
-    const MyPlusCalculator = await ethers.getContractFactory("MyPlusCalculator");
-    const myPlusCalculator = await MyPlusCalculator.connect(myAccount).deploy(); 
-    await myPlusCalculator.deployed();
+  const [myAccount] = await ethers.getSigners();
 
-    const instance = "0x000...000"; // 이곳에 덧셈 문제 인스턴스 컨트랙트 주소를 넣으세요
-    const PlusCalculatorProblem = await ethers.getContractFactory("PlusCalculatorProblem");
-    const plusCalculatorProblem = PlusCalculatorProblem.attach(instance);
-    plusCalculatorProblem.connect(myAccount).setPlusCalculator(myPlusCalculator.address);
+  const MyPlusCalculator = await ethers.getContractFactory("MyPlusCalculator");
+  const myPlusCalculator = await MyPlusCalculator.connect(myAccount).deploy();
+  await myPlusCalculator.deployed();
+
+  const instance = "0x448e225b4814260467CAF89cEAaa593127AaC7A2";
+  const PlusCalculatorProblem = await ethers.getContractFactory(
+    "PlusCalculatorProblem"
+  );
+  const plusCalculatorProblem = PlusCalculatorProblem.attach(instance);
+  plusCalculatorProblem
+    .connect(myAccount)
+    .setPlusCalculator(myPlusCalculator.address);
 }
 
 async function main() {
-    calculatorSol();
+  calculatorSol();
 }
 
 main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
+  console.error(error);
+  process.exitCode = 1;
 });
